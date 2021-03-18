767204e172eb524839bc44a7287dad2f2f9cf3c3
praiakov <adrianopraia@live.com>
2021-03-18 07:46:39 -0300

    refactor(module): change module

--- doc/2 - createModule/greetings/greetings.go
+++ doc/2 - createModule/greetings/greetings.go
@@ -16,6 +16,21 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+
+		messages[name] = message
+	}
+
+	return messages, nil
+}
+
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
--- doc/2 - createModule/hello/hello.go
+++ doc/2 - createModule/hello/hello.go
@@ -11,7 +11,9 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Praia")
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	message, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
