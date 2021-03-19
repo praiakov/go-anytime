cb0daf4b6d5fb26ba9a34bcc1c149d38cb1eae82
praiakov <adrianopraia@live.com>
2021-03-19 07:51:33 -0300

    feat(const): example constants

---
+++ doc/3 - a tour of Go/5 - Const/constants.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	const Pi = 3.14
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+}
