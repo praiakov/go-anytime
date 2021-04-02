5586ab4a8c9454954d152f1557581a5ea7eb9359
praiakov <adrianopraia@live.com>
2021-04-02 09:54:49 -0300

    conectando banco de dados

---
+++ alura/web/go.mod
@@ -0,0 +1,5 @@
+module example.com/main
+
+go 1.16
+
+require github.com/lib/pq v1.10.0
--- alura/web/main.go
+++ alura/web/main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
+func conectaComBancoDeDados() *sql.DB {
+	conexao := "user=postgres dbname=alura password=macacoaurelio sslmode=disable"
+
+	db, err := sql.Open("postgres", conexao)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
+
 type Produto struct {
+	Id              int
 	Nome, Descricao string
 	Preco           float64
 	Quantidade      int
@@ -14,13 +29,41 @@ type Produto struct {
 var temple = template.Must(template.ParseGlob("templates/*.html"))
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{Nome: "Camiseta", Descricao: "Camisa Hering", Preco: 39.99, Quantidade: 5},
-		{Nome: "Notebook", Descricao: "Dell", Preco: 1939.99, Quantidade: 2},
-		{"Tenis", "Nike", 399.99, 3},
+	db := conectaComBancoDeDados()
+
+	selectProdutos, err := db.Query("Select * from produtos")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selectProdutos.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Id = id
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
+
 	}
 
 	temple.ExecuteTemplate(w, "Index", produtos)
+
+	defer db.Close()
 }
 
 func main() {
