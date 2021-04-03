cbcf161ff2aa9e8d6e0fa8cd55e976840cb32204
praiakov <adrianopraia@live.com>
2021-04-03 10:56:14 -0300

    deletando os produtos

--- alura/web/models/produtos.go
+++ alura/web/models/produtos.go
@@ -60,3 +60,17 @@ func CriarNovoProduto(nome, descricao string, preco float64, quantidade int) {
 	defer db.Close()
 
 }
+
+func DeletaProduto(id string) {
+	db := db.ConectaComBancoDeDados()
+
+	deletarProduto, err := db.Prepare("delete from produtos where id = $1")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	deletarProduto.Exec(id)
+	defer db.Close()
+
+}
