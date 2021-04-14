d0ee0850f0b3e8512dbf08b039e17d3b63407fdc
praiakov <adrianopraia@live.com>
2021-04-14 08:20:34 -0300

    benchmark

---
+++ benchmark/go.mod
@@ -0,0 +1,3 @@
+module github.com/praiakov/perfomance/bench
+
+go 1.16
---
+++ benchmark/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Calculate(x int) int {
+	result := x + 2
+	return result
+}
+
+func main() {
+	fmt.Println("Hello World")
+}
