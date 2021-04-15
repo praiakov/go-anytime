903183a24b6de4577364761983323b0bc3496268
praiakov <adrianopraia@live.com>
2021-04-15 08:26:53 -0300

    cap1

---
+++ go-book-novatec/cap1/dup1.go
@@ -0,0 +1,26 @@
+// Dup1 exibe o texto de toda linha que aparece mais de
+// uma vez na entrada-padrão, precedida por sua contagem
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+
+	//Nota: ignorando erros em potencial de input.Err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
---
+++ go-book-novatec/cap1/dup2.go
@@ -0,0 +1,40 @@
+// Dup1 exibe o texto de toda linha que aparece mais de
+// uma vez na entrada-padrão, precedida por sua contagem
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignorando erros em potencial de input.Err()
+}
