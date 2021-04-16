04ffe030495d4ff4314155fbf629367b4782aa2d
praiakov <adrianopraia@live.com>
2021-04-16 08:06:49 -0300

    cap1

---
+++ go-book-novatec/cap1/lissajous.go
@@ -0,0 +1,63 @@
+// Lissajous gera animações GIF de figuras de Lissajous aleatórias
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+)
+
+var green = color.RGBA{0x55, 0x55, 0x00, 0xff}
+var colorX = color.RGBA{0x44, 0x00, 0xcc, 0xff}
+
+var palette = []color.Color{green, color.Black, colorX, color.White}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	if len(os.Args) > 1 && os.Args[1] == "web" {
+		//!+http
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			lissajous(w)
+		}
+		http.HandleFunc("/", handler)
+		//!-http
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+		return
+	}
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // número de revoluções completas do oscilador x
+		res     = 0.001 // resolução angular
+		size    = 100   // canvas da imagem cobre de [-size..+size]
+		nframes = 64    // número de quadros da animação
+		delay   = 8     // tempo entre quadros em unidades de 10ms
+	)
+	freq := rand.Float64() * 3.0 // frequência relativa do oscilador y
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // diferença de fase
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				uint8(i))
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTA: ignorando erros de codificação
+}
