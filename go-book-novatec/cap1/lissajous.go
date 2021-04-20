2524e530d3231c60319bb3a2447556f877cf5bef
praiakov <adrianopraia@live.com>
2021-04-20 18:10:15 -0300

    cap1

--- go-book-novatec/cap1/lissajous.go
+++ go-book-novatec/cap1/lissajous.go
@@ -11,6 +11,7 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -25,31 +26,37 @@ func main() {
 	if len(os.Args) > 1 && os.Args[1] == "web" {
 		//!+http
 		handler := func(w http.ResponseWriter, r *http.Request) {
-			lissajous(w)
+			id, err := strconv.Atoi(r.URL.Query().Get("id"))
+			if err != nil || id < 1 {
+				http.NotFound(w, r)
+				return
+			}
+			lissajous(w, float64(id))
 		}
 		http.HandleFunc("/", handler)
 		//!-http
 		log.Fatal(http.ListenAndServe("localhost:8000", nil))
 		return
 	}
-	lissajous(os.Stdout)
+	//lissajous(os.Stdout)
 }
 
-func lissajous(out io.Writer) {
+func lissajous(out io.Writer, r float64) {
 	const (
-		cycles  = 5     // número de revoluções completas do oscilador x
+		//cycles  = 5     // número de revoluções completas do oscilador x
 		res     = 0.001 // resolução angular
 		size    = 100   // canvas da imagem cobre de [-size..+size]
 		nframes = 64    // número de quadros da animação
 		delay   = 8     // tempo entre quadros em unidades de 10ms
 	)
+
 	freq := rand.Float64() * 3.0 // frequência relativa do oscilador y
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0 // diferença de fase
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
+		for t := 0.0; t < r*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
