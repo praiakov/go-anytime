76a60d94920fe7da1739d05f08e924ce7967818d
praiakov <adrianopraia@live.com>
2021-05-04 19:22:38 -0300

    cap4

---
+++ go-book-novatec/cap4/reverse.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+func main() {
+	a := [...]int{1, 2, 3, 4, 5, 6}
+	reverse(a[:])
+	fmt.Println(a)
+}
