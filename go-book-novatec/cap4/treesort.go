0d3bab70cd90f79f9c29ab16e2327de2295e3b8b
praiakov <adrianopraia@live.com>
2021-05-07 08:00:48 -0300

    cap4

---
+++ go-book-novatec/cap4/treesort.go
@@ -0,0 +1,41 @@
+package treesort
+
+type Tree struct {
+	value       int
+	left, right *Tree
+}
+
+// Sort sorts values in place.
+func Sort(values []int) {
+	var root *Tree
+	for _, v := range values {
+		root = add(root, v)
+	}
+	appendValues(values[:0], root)
+}
+
+// appendValues appends the elements of t to values in order
+// and returns the resulting slice.
+func appendValues(values []int, t *Tree) []int {
+	if t != nil {
+		values = appendValues(values, t.left)
+		values = append(values, t.value)
+		values = appendValues(values, t.right)
+	}
+	return values
+}
+
+func add(t *Tree, value int) *Tree {
+	if t == nil {
+		// Equivalent to return &tree{value: value}.
+		t = new(Tree)
+		t.value = value
+		return t
+	}
+	if value < t.value {
+		t.left = add(t.left, value)
+	} else {
+		t.right = add(t.right, value)
+	}
+	return t
+}
