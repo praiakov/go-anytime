26b305b957d9d2d26aa4709fcc3332a3acbd632e
praiakov <adrianopraia@live.com>
2021-05-15 14:31:02 -0300

    cap5

---
+++ go-book-novatec/cap5/findlinks/findlinks1.go
@@ -0,0 +1,38 @@
+// Findlinks1 exibe os links de um documento HTML lido na entrada-padrão
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+// vist concatena cada link encontra em n <a> links ee devolve o resultado
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+
+	return links
+}
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
---
+++ go-book-novatec/cap5/findlinks/go.mod
@@ -0,0 +1,5 @@
+module tidy
+
+go 1.16
+
+require golang.org/x/net v0.0.0-20210510120150-4163338589ed // direct
