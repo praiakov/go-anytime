881ebd796bbf841d132b653b72adb5c974f89447
praiakov <adrianopraia@live.com>
2021-07-07 23:18:16 -0300

    cap9

---
+++ go-book-novatec/cap9/memo2/memo2.go
@@ -0,0 +1,31 @@
+package memo
+
+import "sync"
+
+type Memo struct {
+	f     Func
+	mu    sync.Mutex // guarda cache
+	cache map[string]result
+}
+
+type Func func(key string) (interface{}, error)
+type result struct {
+	value interface{}
+	err   error
+}
+
+func New(f Func) *Memo {
+	return &Memo{f: f, cache: make(map[string]result)}
+}
+
+// Get é seguro para concorrência
+func (memo *Memo) Get(key string) (interface{}, error) {
+	memo.mu.Lock()
+	res, ok := memo.cache[key]
+	if !ok {
+		res.value, res.err = memo.f(key)
+		memo.cache[key] = res
+	}
+	memo.mu.Unlock()
+	return res.value, res.err
+}
