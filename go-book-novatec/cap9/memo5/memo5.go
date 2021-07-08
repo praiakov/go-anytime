a05e1f099592580f57c7a98539fa2efc81be99ce
praiakov <adrianopraia@live.com>
2021-07-08 19:05:17 -0300

    cap9

---
+++ go-book-novatec/cap9/memo5/memo5.go
@@ -0,0 +1,63 @@
+package memo
+
+type entry struct {
+	res   result
+	ready chan struct{} // closed when res is ready
+}
+
+// Um request é uma mensagem que solicita que Func seja aplicada a key
+type request struct {
+	key      string
+	response chan<- result // o cliente quer um único resultado
+}
+
+type Memo struct{ requests chan request }
+
+type Func func(key string) (interface{}, error)
+type result struct {
+	value interface{}
+	err   error
+}
+
+func New(f Func) *Memo {
+	memo := &Memo{requests: make(chan request)}
+	go memo.server(f)
+	return memo
+}
+
+func (memo *Memo) Get(key string) (interface{}, error) {
+	response := make(chan result)
+	memo.requests <- request{key, response}
+	res := <-response
+	return res.value, res.err
+}
+
+func (memo *Memo) Close() { close(memo.requests) }
+
+func (memo *Memo) server(f Func) {
+	cache := make(map[string]*entry)
+	for req := range memo.requests {
+		e := cache[req.key]
+		if e == nil {
+			// This is the first request for this key.
+			e = &entry{ready: make(chan struct{})}
+			cache[req.key] = e
+			go e.call(f, req.key) // call f(key)
+		}
+		go e.deliver(req.response)
+	}
+}
+
+func (e *entry) call(f Func, key string) {
+	// Evaluate the function.
+	e.res.value, e.res.err = f(key)
+	// Broadcast the ready condition.
+	close(e.ready)
+}
+
+func (e *entry) deliver(response chan<- result) {
+	// Wait for the ready condition.
+	<-e.ready
+	// Send the result to the client.
+	response <- e.res
+}
