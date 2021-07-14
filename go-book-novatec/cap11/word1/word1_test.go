dce3a83b1d852a1b9467cb0f8a95277289338f38
praiakov <adrianopraia@live.com>
2021-07-14 20:07:47 -0300

    cap11

--- go-book-novatec/cap11/word1/word1_test.go
+++ go-book-novatec/cap11/word1/word1_test.go
@@ -16,3 +16,16 @@ func TestNonPalindrome(t *testing.T) {
 		t.Errorf(`IsPalindrome("palindrome") = true`)
 	}
 }
+
+func TestFrenchPalindrome(t *testing.T) {
+	if !IsPalindrome("été") {
+		t.Errorf(`IsPalindrome("été") = false`)
+	}
+}
+
+func TestCanalPalindrome(t *testing.T) {
+	input := "A man, a plan, a canal: Panama"
+	if !IsPalindrome(input) {
+		t.Errorf(`IsPalindrome(%q) = false`, input)
+	}
+}
