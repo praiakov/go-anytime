685821b54a3b25a185a49ec9901ba2887c688d91
praiakov <adrianopraia@live.com>
2021-07-18 15:59:31 -0300

    cap11

---
+++ go-book-novatec/cap11/echo/echo.go
@@ -0,0 +1,34 @@
+//echo exibe seus argumentos de linha de comando
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+var (
+	n = flag.Bool("n", false, "omit trailing newline")
+	s = flag.String("s", " ", "separator")
+)
+
+var out io.Writer = os.Stdout // modificado durante os testes
+
+func main() {
+	flag.Parse()
+	if err := echo(!*n, *s, flag.Args()); err != nil {
+		fmt.Fprintf(os.Stderr, "echo: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func echo(newline bool, sep string, args []string) error {
+	fmt.Fprint(out, strings.Join(args, sep))
+	if newline {
+		fmt.Fprintln(out)
+	}
+
+	return nil
+}
