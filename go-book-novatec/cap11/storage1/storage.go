de50513af97436f54523bb353bd5713c2d373b30
praiakov <adrianopraia@live.com>
2021-07-19 21:24:01 -0300

    cap11

---
+++ go-book-novatec/cap11/storage1/storage.go
@@ -0,0 +1,34 @@
+package storage
+
+import (
+	"fmt"
+	"log"
+	"net/smtp"
+)
+
+var usage = make(map[string]int64)
+
+func bytesInUse(username string) int64 { return usage[username] }
+
+// Configuração para envio de e-mail
+// NOTA: jamais coloque senhas no código-fonte
+const sender = "notifications@example.com"
+const password = "correcthorsebatterystaple"
+const hostname = "smtp.example.com"
+
+const template = `Warning: you are using %d bytes of storage, %d%% of your quota`
+
+func CheckQuota(username string) {
+	used := bytesInUse(username)
+	const quota = 1000000000 // 1GB
+	percent := 100 * used    // quota
+	if percent < 90 {
+		return // OK
+	}
+	msg := fmt.Sprintf(template, used, percent)
+	auth := smtp.PlainAuth("", sender, password, hostname)
+	err := smtp.SendMail(hostname+":587", auth, sender, []string{username}, []byte(msg))
+	if err != nil {
+		log.Printf("smt.Sendmail(%s) failed: %s", username, err)
+	}
+}
