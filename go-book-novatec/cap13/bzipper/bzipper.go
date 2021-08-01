3ef2bf65dc79fcb304e19bc384c14b82f19870c3
praiakov <adrianopraia@live.com>
2021-08-01 13:27:02 -0300

    cap13

---
+++ go-book-novatec/cap13/bzipper/bzipper.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+
+	"gopl.io/ch13/bzip"
+)
+
+func main() {
+	w := bzip.NewWriter(os.Stdout)
+	if _, err := io.Copy(w, os.Stdin); err != nil {
+		log.Fatalf("bzipper: %v\n", err)
+	}
+	if err := w.Close(); err != nil {
+		log.Fatalf("bzipper: close: %v\n", err)
+	}
+}
