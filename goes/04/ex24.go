aa4aad93110cef93edec234e6d81904196d7e787
praiakov <adrianopraia@live.com>
2021-11-12 08:54:31 -0300

    ex24

---
+++ goes/04/ex24.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+
+	fmt.Println(x[:5])
+	fmt.Println(x[5:])
+	fmt.Println(x[2:7])
+	fmt.Println(x[1:6])
+	fmt.Println(x[1:6])
+}
